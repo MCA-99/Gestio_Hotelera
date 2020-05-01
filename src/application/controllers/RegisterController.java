80b6c50c6db40a8f0ed79662ce6784b0db1c4946
MCA-99 <capdet2@gmail.com>
2020-05-01 13:53:12 +0200

    Register

---
+++ src/application/controllers/RegisterController.java
@@ -0,0 +1,89 @@
+package application.controllers;
+
+import java.io.File;
+
+import application.models.Database;
+import javafx.scene.media.MediaView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+	
+	private Database db = Database.getDatabase();
+	@FXML MediaView bgVideo;
+    String path = new File("src/bg/bg.mp4").getAbsolutePath();
+	Media me = new Media(new File(path).toURI().toString());
+	MediaPlayer md = new MediaPlayer(me);
+
+    @FXML
+    private TextField nom_usuari;
+
+    @FXML
+    private PasswordField contrasenya;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField cognom1;
+
+    @FXML
+    private TextField cognom2;
+
+    @FXML
+    private TextField dni;
+
+    @FXML
+    private TextField passaport;
+
+    @FXML
+    private TextField nacionalitat;
+
+    @FXML
+    private TextField telefon;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField estat_civil;
+
+    @FXML
+    private TextField ocupacio;
+
+    @FXML
+    private ChoiceBox<String> rol_list;
+
+    @FXML
+    void register(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void initialize() {
+    	md.setOnEndOfMedia(new Runnable() {
+			@Override
+			public void run() {
+				md.seek(Duration.ZERO);
+				md.play();
+			}
+		});
+    	
+    	bgVideo.setMediaPlayer(md);
+    	md.setAutoPlay(true);
+    	md.setVolume(0);
+    }
+    
+    @FXML
+    void register_button(ActionEvent event) {
+    	
+    }
+
+}
+
